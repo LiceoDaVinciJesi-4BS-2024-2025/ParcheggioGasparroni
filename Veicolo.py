80a378ef38d337818412934d1364cca8bd93228c
Gabriele Gasparroni <s11082772@liceodavincijesi.edu.it>
2025-01-16 08:42:40 +0100

    veicolocomp

--- Veicolo.py
+++ Veicolo.py
@@ -1,15 +1,38 @@
 listamarche = ["Ferrari","Lamborghini","Bugatti","McLaren","Rolls Royce","Bentley","Koenigsegg","Maybach"]
 listacolore = ["nero","rosso","giallo","verde","carbonio"]
 listaalimentazione = ["benzina","diesel","elettrico","ibrido"]
-
+alfabeto = "QWERTYUIOPASDFGHJKLZXCVBNM"
+numeri = "1234567890"
 
 class Veicolo:
     def __init__(self,marca:str,modello:str,colore:str,cilindrata:int,alimentazione:str,targa:str):
+        
+        if marca not in listamarche:
+            raise ValueError ("Marca non borghese")
+        
         self.__marca = marca
+        
         self.__modello = modello
+        
+        if colore not in listacolore:
+            raise ValueError ("Colore non accettato")
+        
         self.__colore = colore
+
+        if (cilindrata % 100) != 0:
+            raise ValueError ("Cilindrata non accettata")
+        
         self.__cilindrata = cilindrata
-        self.__alimentazione = alimentazione
+        
+        if alimentazione not in listaalimentazione:
+            raise ValueError ("Alimentazione non accettata")
+            
+            self.__alimentazione = alimentazione
+        
+        if len(targa) != 7 or targa[0] not in alfabeto or targa[1] not in alfabeto or targa[2] not in numeri or targa[3] not in numeri or targa[4] not in numeri or targa[5] not in alfabeto or targa[6] not in alfabeto:
+            
+            raise ValueError("ERRORE")
+        
         self.__targa = targa
     
     def __str__(self):
@@ -22,13 +45,13 @@ class Veicolo:
     
     @targa.setter
     def targa(self,value):
-        alfabeto = "QWERTYUIOPASDFGHJKLZXCVBNM"
-        numeri = "1234567890"
-        if len(value) != 7 or value[0] not in alfabeto or value[1] not in alfabeto or value[2] not in numeri or value[3] not in numeri or value[4] not in numeri or value[5] not in alfabeto or value[6] not in alfabeto:
-            raise ValueError("lunghezza targa errata")
+        
+        if len(targa) != 7 or targa[0] not in alfabeto or targa[1] not in alfabeto or targa[2] not in numeri or targa[3] not in numeri or targa[4] not in numeri or targa[5] not in alfabeto or targa[6] not in alfabeto:
+            
+            raise ValueError("ERRORE")
         
         self.__targa = value
-
+        
         return
     
     @property
@@ -48,7 +71,7 @@ class Veicolo:
     def colore(self):
         return self.__colore
     
-    @marca.setter
+    @colore.setter
     def colore(self,value):
         if colore not in listacolore:
             raise ValueError ("Colore non accettato")
@@ -61,7 +84,7 @@ class Veicolo:
     def alimentazione(self):
         return self.__alimentazione
     
-    @marca.setter
+    @alimentazione.setter
     def alimentazione(self,value):
         if alimentazione not in listaalimentazione:
             raise ValueError ("Alimentazione non accettata")
@@ -74,7 +97,7 @@ class Veicolo:
     def cilindrata(self):
         return self.__cilindrata
     
-    @marca.setter
+    @cilindrata.setter
     def cilindrata(self,value):
         if (cilindrata % 100) != 0:
             raise ValueError ("Cilindrata non accettata")
@@ -87,5 +110,17 @@ class Veicolo:
     def modello(self):
         return self.__modello
     
-    def __lt__(self,other):
-        
+    def __le__ (self,other):
+        if self.__marca <= other.marca:
+            return True
+        elif self.__modello <= other.modello:
+            return True
+        elif self.__cilindrata <= other.cilindrata:
+            return True
+    
+if __name__ == "__main__":
+    v1 = Veicolo("Ferrari", "Sf90", "rosso", 6000, "benzina", "BG999JW")
+    print(v1)
+    v2 = Veicolo("Lamborghini","Aventador SVJ","nero",7200,"benzina","GG009BB")
+    print(v2)
+    print(v1.__le__(v2))

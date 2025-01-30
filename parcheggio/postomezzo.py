40735cb35eb69eba4ea9ead7ebb8525d3f0c19b4
Gabriele Gasparroni <s11082772@liceodavincijesi.edu.it>
2025-01-30 09:06:55 +0100

    controllooccupazione

--- parcheggio/postomezzo.py
+++ parcheggio/postomezzo.py
@@ -30,6 +30,8 @@ class PostoMezzo:
             self.__dataPartenza = dataPartenza
             self.__targa = targa
             return ("Posto preso")
+        if self.__posto == True:
+            raise ValueError("Posto occupato")
 
     def liberaPosto (self):
         if self.__posto == True:
@@ -42,10 +44,13 @@ class PostoMezzo:
 
 if __name__ == "__main__":
     a1 = Auto("Ferrari", "Sf90", "rosso", 6000, "benzina", "BG999JW", 2, 1, 200, 70)
+    a2 = Auto("Lamborghini","Aventador","nero",5400,"benzina","GG300CK", 2, 2, 250, 130)
     posto = PostoMezzo()
     print(posto)
     print(posto.occupaPosto(a1,datetime.datetime(2025, 7, 20, 20, 18, 00),"BG999JW"))
     print(posto)
+#    print(posto.occupaPosto(a2,datetime.datetime(2025, 8, 17, 21, 00, 00),"GG300CK"))
+#    print(posto)
     print(posto.liberaPosto())
     print(posto)
 
